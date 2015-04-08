4cfd679dcb185022155c698fbf3b9995123a0af1
CherryCoke <tbonnette25@gmail.com>
2015-04-08 15:11:47 -0700

    Committing Version 0.2.4

--- src/data/TileGrid.java
+++ src/data/TileGrid.java
@@ -21,6 +21,24 @@ public class TileGrid {
 		
 	}
 	
+	/*
+	 TileGrid is the "level editor" so to say where the texture draw is
+	 on-screen will change depending on if (in this case) 0 or a 
+	 different value is typed into the double array (see Boot.java)
+	 */
+	public TileGrid(int[][] newMap){
+		map = new Tile[22][13];
+		for (int i = 0; i < map.length; i++){
+			for (int j = 0; j < map[i].length; j++){
+				if (newMap[j][i] == 0)
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
+				else
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Dirt);
+					
+			}
+		}
+	}
+	
 	public void Draw(){
 		//Drawing textures to each tile in the grid
 		for (int i= 0; i < map.length; i ++){
