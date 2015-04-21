a4696df7ec1a60cb415d0337106725fb0f3c4f72
CherryCoke <tbonnette25@gmail.com>
2015-04-20 20:08:16 -0700

    ACTUALLY committing version 0.2.5

--- src/data/TileGrid.java
+++ src/data/TileGrid.java
@@ -30,13 +30,33 @@ public class TileGrid {
 		map = new Tile[22][13];
 		for (int i = 0; i < map.length; i++){
 			for (int j = 0; j < map[i].length; j++){
-				if (newMap[j][i] == 0)
+				/*
+				  Because the newMap variable is always the one being changed, 
+				  a switch statement is being used rather than an if/else to 
+				  avoid extra/redundant code
+				 */
+				switch (newMap[j][i]){
+				case 0:
 					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
-				else
+					break;
+				case 1:
 					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Dirt);
+					break;
+				case 2:
+					map[i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Water);
+					break;
 					
+				}
 			}
 		}
+	}
+	
+	public void setTile(int xCoord, int yCoord, TileType type){
+		map[xCoord][yCoord] = new Tile(xCoord * 64, yCoord * 64, 64, 64, type);
+	}
+	
+	public Tile getTile(int xCoord, int yCoord){
+		return map[xCoord][yCoord];
 	}
 	
 	public void Draw(){
--- src/data/TileType.java
+++ src/data/TileType.java
@@ -5,12 +5,11 @@ public enum TileType {
 	/*
 	Creating a new enumerator. Enums are useful when
 	you have a method or instances where you want only a 
-	small possible set of values. In this case, I only want
-	tiles to be grass or dirt, because I only have 
-	grass and dirt textures (and they each have a specific
-	property assigned to them). 
+	small possible set of values. In this case, the only 
+	values are a texture followed by a boolean value for 
+	if towers can be built upon them
 	*/
-	Grass("grass64", true), Dirt("dirt64", false);
+	Grass("grass64", true), Dirt("dirt64", false), Water("water64", false);
 	
 	String textureName;
 	boolean buildable;
